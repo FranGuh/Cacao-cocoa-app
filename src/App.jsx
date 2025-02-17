ae021cbc712133df5d6242600dfe81e8460bad64
Gustavo Francisco <franguho@gmail.com>
2025-02-17 13:51:32 -0600

    Unificación parte de cristian

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Navbar, BlogCard, VideoCard, TextContainer, ProductCard, FooterBar, Ima
 import './App.css';
 import videoCocoa from './assets/video/video_cacao.mp4';
 import { BlogSection } from './components';
+import videoChocolate from './assets/video/chocolate.mp4';
 
 
 const App = () => {
@@ -35,6 +36,7 @@ const App = () => {
             source="@cacao.cocoa_with_cause"
           />
         </div>
+
         <div>
           <TextContainer 
             text="Productos Gluten Free!"
@@ -43,12 +45,26 @@ const App = () => {
         <div className='content-display__picture content-pattern-background'>
           <ProductCard />
         </div>
+
+
+        <div className='content-display__video video-container'>
+          <VideoCard
+            videoSource={videoChocolate}
+          />
+          <div className='video-text-overlay'>
+            <h2>El mejor chocolate</h2>
+            <p>Disfruta de cada bocado</p>
+          </div>
+        </div>
+        
         <div>
 
           {/* A partir de aqui trabajen el blog */}
           <TextContainer 
             text="Mi blog!"
           />
+
+        
 
         <div className="background-div">
           <img 
@@ -59,19 +75,46 @@ const App = () => {
           <h1 className="elegant-title">Soy Carla Olivares</h1>
         </div>
 
-          {/* Ejemplo como colocar el blog */}
-          {/*<ImageCard 
-            text="hola"
-            nameText="Mi primer articulo"
-            logo="c.png"
-          />*/}
         <div className='content-pattern-background'>
           <BlogSection />
           <BlogSection />
-          <BlogSection />
-                   
+          <BlogSection />  
         </div>                  
         </div>
+
+        <div className='content-pattern-background2'>
+          <TextContainer 
+            text="Motivate!"
+          />
+          <div className='content-display'>
+            <ImageCard 
+              nameText="Hecho a mano con amor"
+              logo="/Imagenes/productos/3.jpeg"
+            />
+            <ImageCard 
+              nameText="Te mereces un regalo…un dulce y delicioso chocolate "
+              logo="/Imagenes/productos/2.jpeg"
+            />
+            <ImageCard 
+              nameText="Todos merecemos un momento de placer, sin un momento de culpa"
+              logo="/Imagenes/productos/4.jpeg"
+            />
+            <ImageCard 
+              nameText="Chocolates que unen, no que excluyen"
+              logo="/Imagenes/productos/5.jpeg"
+            />
+            <ImageCard 
+              nameText="Nuestro chocolate es…El momento de golosina de calidad que te mereces"
+              logo="/Imagenes/productos/6.jpeg"
+            />
+            <ImageCard 
+              nameText="Complace tus pasiones"
+              logo="/Imagenes/productos/6.jpeg"
+            />
+          </div>
+        </div>
+        
+
       </div>
       <FooterBar />
     </div>
