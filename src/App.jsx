b4234b9a6d29639d168846ac471001fe1d4873d9
Gustavo Francisco <franguho@gmail.com>
2025-02-18 12:24:04 -0600

    seccionada la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { React } from 'react';
+import { useState } from 'react';
 import '/src/assets/bootstrap.min.css';
 import { Navbar, BlogCard, VideoCard, TextContainer, ProductCard, FooterBar, ImageCard } from './components';
 import './App.css';
@@ -8,120 +8,136 @@ import videoChocolate from './assets/video/chocolate.mp4';
 
 
 const App = () => {
+  const [seccionActiva, setSeccionActiva] = useState('inicio');
   
   return (
     <div>
-      <Navbar />
+      <Navbar setSeccionActiva={setSeccionActiva}/>
       <div>
-        <div className='content-display__video'>
-          <VideoCard 
-            videoSource={videoCocoa}
-          />
-          <div className='video-text-overlay'>
-            <h2>Cacao.cocoa</h2>
-            <p>Un chocolate, una sensación</p>
+
+        {seccionActiva === 'inicio' && (
+        <section id="inicio">
+          <div className='content-display__video'>
+            <VideoCard 
+              videoSource={videoCocoa}
+            />
+            <div className='video-text-overlay'>
+              <h2>Cacao.cocoa</h2>
+              <p>Un chocolate, una sensación</p>
+            </div>
           </div>
-        </div>
 
-        <div className='content-display'>
-          <BlogCard 
-            text="Chocolates PREMIUM y libres de GLUTEN"
-            autor="cacao.cocoa"
-            source="@cacao.cocoa_with_cause"
-          />
-          <BlogCard 
-            text="Pasión al alcance de un bocadillo sano"
-            autor="cacao.cocoa2"
-            source="@cacao.cocoa_with_cause"
-          />
-          <BlogCard 
-            text="Para el paladar más exigente"
-            autor="cacao.cocoa3"
-            source="@cacao.cocoa_with_cause"
-          />
-        </div>
-
-        <div>
-          <TextContainer 
-            text="Productos Gluten Free!"
-          />
-        </div>
-        
-        <div className='content-pattern-background'>
-          <ProductCard />
-        </div>
-
-        
-        <div>
-
-          {/* A partir de aqui trabajen el blog */}
-          <TextContainer 
-            text="Newsletter!"
-          />
-
-        
-
-        <div className="background-div">
-          <img 
-          src="/Imagenes/productos/1.png" 
-          alt="Producto 1" 
-          style={{ width: '600px', borderRadius: '10px' }} 
-          />
-          <h1 className="elegant-title">Soy Carla Olivares</h1>
-        </div>
-
-        <div className='content-pattern-background'>
-          <BlogSection />
-          <BlogSection />
-          <BlogSection />  
-        </div>                  
-        </div>
-
-        <div className='content-pattern-background2'>
-          <TextContainer 
-            text="Motivate!"
-          />
-          <div className='content-display__blog'>
-            <ImageCard 
-              nameText="Hecho a mano con amor"
-              logo="/Imagenes/productos/3.jpeg"
+          <div className='content-display'>
+            <BlogCard 
+              text="Chocolates PREMIUM y libres de GLUTEN"
+              autor="cacao.cocoa"
+              source="@cacao.cocoa_with_cause"
             />
-            <ImageCard 
-              nameText="Te mereces un regalo…un dulce y delicioso chocolate "
-              logo="/Imagenes/productos/2.jpeg"
+            <BlogCard 
+              text="Pasión al alcance de un bocadillo sano"
+              autor="cacao.cocoa2"
+              source="@cacao.cocoa_with_cause"
             />
-            <ImageCard 
-              nameText="Todos merecemos un momento de placer, sin un momento de culpa"
-              logo="/Imagenes/productos/4.jpeg"
-            />
-            <ImageCard 
-              nameText="Chocolates que unen, no que excluyen"
-              logo="/Imagenes/productos/5.jpeg"
-            />
-            <ImageCard 
-              nameText="Nuestro chocolate es…El momento de golosina de calidad que te mereces"
-              logo="/Imagenes/productos/6.jpeg"
-            />
-            <ImageCard 
-              nameText="Complace tus pasiones"
-              logo="/Imagenes/productos/6.jpeg"
+            <BlogCard 
+              text="Para el paladar más exigente"
+              autor="cacao.cocoa3"
+              source="@cacao.cocoa_with_cause"
             />
           </div>
-        </div>
-
+        </section>
+        )}
         
-        <div className='content-display__video video-container'>
-          <VideoCard
-            videoSource={videoChocolate}
-          />
-          <div className='video-text-overlay'>
-            <h2>El mejor chocolate</h2>
-            <p>Disfruta de cada bocado</p>
+
+        {seccionActiva === 'productos' && (
+        <section id="productos">
+          <div>
+            <TextContainer 
+              text="Productos Gluten Free!"
+            />
           </div>
-        </div>
-
+          
+          <div className='content-pattern-background'>
+            <ProductCard />
+          </div>
+        </section>
+        )}
         
 
+
+        {seccionActiva === 'newsletter' && (
+        <section id="newsletter">
+          <div>
+            {/* A partir de aqui trabajen el blog */}
+            <TextContainer 
+              text="Newsletter"
+            />
+  
+          <div className="background-div">
+            <img 
+            src="/Imagenes/productos/1.png" 
+            alt="Producto 1" 
+            style={{ width: '600px', borderRadius: '10px' }} 
+            />
+            <h1 className="elegant-title">Soy Carla Olivares</h1>
+          </div>
+  
+          <div className='content-pattern-background'>
+            <BlogSection />
+            <BlogSection />
+            <BlogSection />  
+          </div>                  
+          </div>
+        </section>
+        )}
+
+
+        {seccionActiva === 'motivarme' && (
+          <section id="motivarme">
+          <div className='content-pattern-background2'>
+            <TextContainer 
+              text="Motivate"
+            />
+            <div className='content-display__blog'>
+              <ImageCard 
+                nameText="Hecho a mano con amor"
+                logo="/Imagenes/productos/3.jpeg"
+              />
+              <ImageCard 
+                nameText="Te mereces un regalo…un dulce y delicioso chocolate "
+                logo="/Imagenes/productos/2.jpeg"
+              />
+              <ImageCard 
+                nameText="Todos merecemos un momento de placer, sin un momento de culpa"
+                logo="/Imagenes/productos/4.jpeg"
+              />
+              <ImageCard 
+                nameText="Chocolates que unen, no que excluyen"
+                logo="/Imagenes/productos/5.jpeg"
+              />
+              <ImageCard 
+                nameText="Nuestro chocolate es…El momento de golosina de calidad que te mereces"
+                logo="/Imagenes/productos/6.jpeg"
+              />
+              <ImageCard 
+                nameText="Complace tus pasiones"
+                logo="/Imagenes/productos/6.jpeg"
+              />
+            </div>
+          </div>
+
+          
+          <div className='content-display__video video-container'>
+            <VideoCard
+              videoSource={videoChocolate}
+            />
+            <div className='video-text-overlay'>
+              <h2>El mejor chocolate</h2>
+              <p>Disfruta de cada bocado</p>
+            </div>
+          </div>
+        </section>
+        )}
+        
       </div>
       <FooterBar />
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import logo from '../../assets/logo.png';
 
-const Navbar = () => {
+const Navbar = ({ setSeccionActiva }) => {
   return (
-      <nav className="navbar navbar-expand-lg bg-light-logo" target="_blank" data-bs-theme="light">
+      <nav className="navbar navbar-expand-lg bg-light-logo" data-bs-theme="light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="#" onClick={() => setSeccionActiva('inicio')}>
             <img src={logo} alt='Cacao.Cocoa' height="120px" width="150px"/>
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,18 +12,16 @@ const Navbar = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarColor02">
             <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Home <span className="visually-hidden">(current)</span></a>
-              </li>
-              <li className="nav-item"><a className="nav-link" href="#">Products</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">Blog</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">Redes</a></li>
-            
+              <li className="nav-item"><a className="nav-link active" href="#" onClick={() => setSeccionActiva('inicio')}>Home</a></li>
+              <li className="nav-item"><a className="nav-link" href="#" onClick={() => setSeccionActiva('productos')}>Products</a></li>
+              <li className="nav-item"><a className="nav-link" href="#" onClick={() => setSeccionActiva('newsletter')}>Newsletter</a></li>
+              <li className="nav-item"><a className="nav-link" href="#" onClick={() => setSeccionActiva('motivarme')}>Motivarme</a></li>
             </ul>
           </div>
         </div>
       </nav>
   );
 };
+
 
 export default Navbar;
